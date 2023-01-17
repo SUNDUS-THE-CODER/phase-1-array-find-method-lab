0d590b281c4dfc343abe03a1edfc255b7cc84a9a
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 22:55:52 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const results = record.find((key) => key.result === "W");
+    return typeof results === 'undefined' ? results : results.year;
+}
